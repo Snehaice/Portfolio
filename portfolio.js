9437adcc6ac309390e214cb301fa645cf16ea27e
Snehaice <145124363+Snehaice@users.noreply.github.com>
2025-11-11 13:24:00 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,4 @@
 
-  function toggleMenu() {
-    const navLinks = document.querySelector("nav > ul");
-    const menuIcon = document.querySelector(".menu-icon");
-    navLinks.classList.toggle("active");
-    menuIcon.classList.toggle("active");
-  }
-
-
 document.getElementById("downloadBtn").addEventListener("click", function() {
    
     window.open("./Sheneka_K_Resume.pdf", "_blank");
@@ -21,7 +13,19 @@ document.getElementById("contactMeBtn").addEventListener("click", function() {
     } else {
         // Fallback or alert if the contact section is missing
         console.warn("Contact section with id='contact' not found. Opening email.");
-        window.location.href = "mailto:sheneka@example.com";
+        window.location.href = "mailto:sheneka277@gmail.com";
     }
-
 });
+
+
+function toggleMenu(){
+   document.querySelector("nav ul").classList.toggle("show");
+}
+
+
+function toggleMenu() {
+    const navLinks = document.querySelector("nav > ul");
+    const menuIcon = document.querySelector(".menu-icon");
+    navLinks.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+  }
